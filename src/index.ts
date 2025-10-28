26bd10ee2ac666725e1cdad7a3abe4ece904c14d
sokaina-elhajj <elhajjsokaina@gmail.com>
2025-10-28 07:27:22 +0200

    Added cake builder

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,24 @@
-import path from 'path';
-import { parseCSV } from './parsers/csvParser';
-import { parseJSON } from './parsers/jsonParser';
-import { parseXml } from './parsers/xmlParser';
-import logger from './util/logger';
+import { CakeBuilder } from "./model/builders/cake.builder";
 
 async function main() {
-  // Resolve file paths
-  const csvFile = path.resolve(__dirname, './data/cake-orders.csv');
-  const jsonFile = path.resolve(__dirname, './data/book-orders.json');
-  const xmlFile  = path.resolve(__dirname, './data/toy-orders.xml');
+  const cakeBuilder = new CakeBuilder();
 
-  try {
-    // CSV 
-    const csvProducts = await parseCSV(csvFile);
-    logger.info('===== CSV Products =====');
-    csvProducts.forEach((row, index) => {
-      logger.info(`${index + 1}: ${row.join(', ')}`);
-    });
-
-    // JSON
-    const jsonProducts = await parseJSON(jsonFile);
-    logger.info('===== JSON Products =====');
-    if (Array.isArray(jsonProducts)) {
-      jsonProducts.forEach((item, index) => {
-        logger.info(`${index + 1}: ${JSON.stringify(item)}`);
-      });
-    } else {
-      logger.info(JSON.stringify(jsonProducts, null, 2));
-    }
-
-    // XML
-    const xmlProducts = await parseXml(xmlFile);
-    logger.info('===== XML Products =====');
-    logger.info(JSON.stringify(xmlProducts, null, 2));
-
-  } catch (error: any) {
-    logger.error('Error occurred while parsing files: %o', error);
-  }
+  const cake = cakeBuilder
+    .setType("type")
+    .setFlavor("flavor")
+    .setFilling("filling")
+    .setSize(10)
+    .setLayers(2)
+    .setFrostingType("frostingType")
+    .setFrostingFlavor("frostingFlavor")
+    .setDecorationType("decorationType")
+    .setDecorationColor("decorationColor")
+    .setCustomMessage("customMessage")
+    .setShape("shape")
+    .setAllergies("allergies")
+    .setSpecialIngredients("specialIngredients")
+    .setPackagingType("packagingType")
+    .build();
 }
 
 main();
-
-// import path from 'path';
-// import {parseCSV} from './parsers/csvParser'
-// import logger from './util/logger';
-
-// const filePath = path.resolve(__dirname, './data/Cake orders.csv');
-
-// async function main() {
-//     try {
-//         const products = await parseCSV(filePath)
-//         for (const product of products) {
-//             logger.info(product + '\n');
-//         }
-//     } catch(error) {
-//         logger.error(error)
-//     }
-// }
-
-// main();
