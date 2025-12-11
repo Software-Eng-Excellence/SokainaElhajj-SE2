5d8a1af23c74caeeeded1a9637535732c785b5e5
sokaina-elhajj <elhajjsokaina@gmail.com>
2025-12-11 18:07:14 +0200

    Assignment Eight

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
-import { DBMode } from "repository/Repository.factory";
+import { DBMode } from "../repository/Repository.factory";
 
 
 dotenv.config({ path: path.join(__dirname, '../../.env') });
